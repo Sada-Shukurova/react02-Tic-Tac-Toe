cf69d4d98ac7056ce30ffd3b9a43be450c23f631
Sada <shukurova.sada@gmail.com>
2024-04-03 17:45:37 +0400

    add state for game board

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,15 +7,29 @@ const initialGameBoard = [
 ];
 
 const GameBoard = () => {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
   return (
     <>
       <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => (
+        {gameBoard.map((row, rowIndex) => (
           <li key={rowIndex}>
             <ol>
               {row.map((playerSymbol, colIndex) => (
                 <li key={colIndex}>
-                  <button>{playerSymbol}</button>
+                  <button
+                    onClick={() => handleSelectSquare(rowIndex, colIndex)}
+                  >
+                    {playerSymbol}
+                  </button>
                 </li>
               ))}
             </ol>
