e4820b35054733feb555a1c274c21122d013a8bf
Sada <shukurova.sada@gmail.com>
2024-04-03 17:18:16 +0400

    add game board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
       LOG
     </main>
---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,27 @@
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+const GameBoard = () => {
+  return (
+    <>
+      <ol id="game-board">
+        {initialGameBoard.map((row, rowIndex) => (
+          <li key={rowIndex}>
+            <ol>
+              {row.map((playerSymbol, colIndex) => (
+                <li key={colIndex}>
+                  <button>{playerSymbol}</button>
+                </li>
+              ))}
+            </ol>
+          </li>
+        ))}
+      </ol>
+    </>
+  );
+};
+
+export default GameBoard;
